cc23befd26f2fad05f97951c10453bec7d8d162f
gaisdav <gais.davletkildin@gmail.com>
2024-07-24 22:53:40 +0200

    fixed pwa config and index.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,15 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/learn-chinese/",
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Learn Chinese",
+        short_name: "Learn Chinese",
+        description: "Learn Chinese",
+      },
+    }),
+  ],
 });
